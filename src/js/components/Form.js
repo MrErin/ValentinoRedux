c543cff2c9102e0505f9ef8f8208359ba3cc684d
MrErin <erinmeaker@gmail.com>
2018-10-25 13:50:46 -0500

    it works!

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import uuidv1 from "uuid";
 import { addArticle } from "../actions/index";
